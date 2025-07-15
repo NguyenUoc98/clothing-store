c8d4c17742a14bca4084fc47269018016c79dc62
Nguyễn Văn Ước <uocnv.soict.hust@gmail.com>
2025-07-15 20:40:06 +0700

    add seeder categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
 //            ItemSeeder::class,
             UserSeeder::class,
             ProductSeeder::class,
+            CategoriesSeeder::class,
         ]);
     }
 }
