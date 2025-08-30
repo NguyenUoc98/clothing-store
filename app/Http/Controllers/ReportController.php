a8b39fb596c9854767da8f31f4e15c6b5c5424e8
Nguyễn Văn Ước <uocnv.soict.hust@gmail.com>
2025-08-30 20:15:36 +0700

    feat: update statistic

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,25 +10,72 @@ use App\Enum\PaymentStatus;
 use App\Models\CartItem;
 use App\Models\Order;
 use Carbon\Carbon;
+use Carbon\CarbonInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ReportController extends Controller
 {
     public function revenueReport(Request $request)
     {
-        $startDate  = $request->get('start_date') ?: Carbon::now()->startOfMonth()->toDateString();
-        $endDate    = $request->get('end_date') ?: Carbon::now()->toDateString();
+        $start     = $request->get('start_date');
+        $end       = $request->get('end_date');
+        $dateGroup = $request->get('date_group', 'date');
 
-        $statistics = Order::query()
-            ->selectRaw('SUM(total_price) as money, DATE(updated_at) as date')
-            ->where('status', PaymentStatus::SUCCESS)
-            ->where('updated_at', '>=', Carbon::createFromFormat('Y-m-d', $startDate)->startOfDay())
-            ->where('updated_at', '<=', Carbon::createFromFormat('Y-m-d', $endDate)->endOfDay())
-            ->groupByRaw('DATE(updated_at)')
-            ->orderByDesc('date')
-            ->paginate(5);
+        if (!$start || !$end) {
+            $start = now()->firstOfMonth()->toDateString();
+            $end   = now()->toDateString();
+        }
 
-        return view('reports.revenue', compact('statistics'));
+        $startDate = Carbon::createFromFormat('Y-m-d', $start)->startOfDay();
+        $endDate   = Carbon::createFromFormat('Y-m-d', $end)->endOfDay();
+
+        if ($dateGroup == 'month') {
+            $startDate = $startDate->startOfMonth();
+            $endDate   = $endDate->endOfMonth();
+        } elseif ($dateGroup == 'year') {
+            $startDate = $startDate->startOfYear();
+            $endDate   = $endDate->endOfYear();
+        } elseif ($dateGroup == 'week') {
+            $startDate = $startDate->startOfWeek(CarbonInterface::MONDAY);
+            $endDate   = $endDate->endOfWeek(CarbonInterface::SUNDAY);
+        }
+
+        $index  = $endDate;
+        $result = [];
+        while ($index > $startDate) {
+            $firstTime = match ($dateGroup) {
+                'month' => (clone $index)->startOfMonth()->subSecond(),
+                'date'  => (clone $index)->subDay(),
+                'week'  => (clone $index)->startOfWeek(CarbonInterface::MONDAY),
+                'year'  => (clone $index)->startOfYear(),
+                default => $startDate,
+            };
+
+            $stringDate = match ($dateGroup) {
+                'month' => Str::ucfirst((clone $firstTime)->addDay()->locale('vi')->translatedFormat('F, Y')),
+                'date'  => Str::ucfirst((clone $index)->locale('vi')->translatedFormat('l, d-m-Y')),
+                'year'  => Str::ucfirst((clone $index)->locale('vi')->translatedFormat('\N\ă\m Y')),
+                default => Str::ucfirst((clone $firstTime)->locale('vi')->translatedFormat('l, d/m/Y')).' đến '.
+                    Str::ucfirst((clone $index)->locale('vi')->translatedFormat('l, d/m/Y')),
+            };
+
+            $statistics = Order::query()
+                ->where('status', PaymentStatus::SUCCESS)
+                ->where('updated_at', '>=', $firstTime)
+                ->where('updated_at', '<=', $index)
+                ->sum('total_price');
+
+            $result[$stringDate] = $statistics;
+
+            if ($dateGroup == 'week') {
+                $index->subWeek()->setTime(23, 59, 59);
+            } else {
+                $index = $firstTime;
+            }
+        }
+
+        return view('reports.revenue', compact('result'));
     }
 
     public function productReport()
--- resources/views/reports/revenue.blade.php
+++ resources/views/reports/revenue.blade.php
@@ -4,8 +4,8 @@
 
 @section('icon')
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
-        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"/>
+        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"/>
     </svg>
 @endsection
 @section('title', 'Báo cáo doanh thu')
@@ -19,12 +19,22 @@
             </div>
             <div class="space-y-2 flex flex-col">
                 <label for="end_date" class="text-gray-700 font-medium">Ngày kết thúc</label>
-                <input type="date" name="end_date" id="end_date" class="p-2 border rounded-lg" value="{{ request()->input('start_date', Carbon\Carbon::now()->toDateString()) }}">
+                <input type="date" name="end_date" id="end_date" class="p-2 border rounded-lg" value="{{ request()->input('end_date', Carbon\Carbon::now()->toDateString()) }}">
+            </div>
+
+            <div class="space-y-2 flex flex-col">
+                <label for="date_group" class="text-gray-700 font-medium">Nhóm</label>
+                <select name="date_group" id="date_group" class="p-2 border rounded-lg">
+                    <option value="date" @selected(request()->input('date_group', 'date') == 'date')>Ngày</option>
+                    <option value="week" @selected(request()->input('date_group', 'date') == 'week')>Tuần</option>
+                    <option value="month" @selected(request()->input('date_group', 'date') == 'month')>Tháng</option>
+                    <option value="year" @selected(request()->input('date_group', 'date') == 'year')>Năm</option>
+                </select>
             </div>
 
             <button class="button bg-[#1c1b22] hover:bg-gray-700 text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"/>
                 </svg>
                 Lọc
             </button>
@@ -42,10 +52,10 @@
             </tr>
             </thead>
             <tbody>
-            @forelse ($statistics as $statistic)
+            @forelse ($result as $dateString => $_res)
                 <tr>
-                    <td class="text-center">{{ $statistic->date }}</td>
-                    <td class="whitespace-nowrap !text-nowrap font-bold text-red-600 text-center"><span>{{ number_format($statistic->money, 0, ',', '.') }} VND</span></td>
+                    <td class="text-center">{{ $dateString }}</td>
+                    <td class="whitespace-nowrap !text-nowrap font-bold text-red-600 text-center"><span>{{ number_format($_res, 0, ',', '.') }} VND</span></td>
                 </tr>
             @empty
                 <tr>
@@ -56,6 +66,5 @@
             @endforelse
             </tbody>
         </table>
-        <div class="py-2 px-4">{{ $statistics->links() }}</div>
     </div>
 @endsection
